f2913cde4a97dd2f1732f96e1ec5c69a81580ef1
yossefillospro-jpg <yossefillospro@gmail.com>
2026-01-26 21:38:30 +0200

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,9 @@
+export const PRODUCTS = [
+  { productId: "tshirt", nameHe: "טי שירט", nameFr: "T-shirt", price: 8 },
+  { productId: "shirt", nameHe: "מכופתרת", nameFr: "Chemise", price: 10 },
+  { productId: "pants", nameHe: "מכנס", nameFr: "Pantalon", price: 12 },
+  { productId: "skirt-short", nameHe: "חצאית קצרה", nameFr: "Jupe courte", price: 10 },
+  { productId: "skirt-long", nameHe: "חצאית ארוכה", nameFr: "Jupe longue", price: 12 },
+  { productId: "dress-short", nameHe: "שמלה קצרה", nameFr: "Robe courte", price: 14 },
+  { productId: "dress-long", nameHe: "שמלה ארוכה", nameFr: "Robe longue", price: 16 },
+];
