a54004e1858abb05851dbd5578d1a273adb8f5dc
yossefillospro-jpg <yossefillospro@gmail.com>
2026-02-01 12:58:20 +0200

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,9 +1,9 @@
 export const PRODUCTS = [
-  { productId: "tshirt", nameHe: "טי שירט", nameFr: "T-shirt", price: 8 },
-  { productId: "shirt", nameHe: "מכופתרת", nameFr: "Chemise", price: 10 },
-  { productId: "pants", nameHe: "מכנס", nameFr: "Pantalon", price: 12 },
+  { productId: "tshirt", nameHe: "טי שירט", nameFr: "T-shirt", price: 7 },
+  { productId: "shirt", nameHe: "מכופתרת", nameFr: "Chemise", price: 15 },
+  { productId: "pants", nameHe: "מכנס", nameFr: "Pantalon", price: 11 },
   { productId: "skirt-short", nameHe: "חצאית קצרה", nameFr: "Jupe courte", price: 10 },
-  { productId: "skirt-long", nameHe: "חצאית ארוכה", nameFr: "Jupe longue", price: 12 },
-  { productId: "dress-short", nameHe: "שמלה קצרה", nameFr: "Robe courte", price: 14 },
-  { productId: "dress-long", nameHe: "שמלה ארוכה", nameFr: "Robe longue", price: 16 },
+  { productId: "skirt-long", nameHe: "חצאית ארוכה", nameFr: "Jupe longue", price: 13 },
+  { productId: "dress-short", nameHe: "שמלה קצרה", nameFr: "Robe courte", price: 15 },
+  { productId: "dress-long", nameHe: "שמלה ארוכה", nameFr: "Robe longue", price: 20 },
 ];
